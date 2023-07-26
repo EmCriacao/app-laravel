8ffc1654e3c6798e9684a15126a4579c7660058c
EmCriacao <joao_grds98@hotmail.com>
2023-07-25 22:38:01 -0300

    feat:

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
-use App\Http\Controllers\Admin\{SupportController};
 use App\Http\Controllers\Site\SiteController;
-use Illuminate\Support\Facades\Route;
 
-Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
+use Illuminate\Support\Facades\Route;
 
 Route::get('/contato', [SiteController::class, 'contact']);
 
